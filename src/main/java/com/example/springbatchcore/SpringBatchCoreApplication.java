d496c3fe9b19a4e75c7eef43c89a599f5d133da3
csupreme19 <csupreme19@gmail.com>
2023-05-18 17:26:55 +0900

    init

---
+++ src/main/java/com/example/springbatchcore/SpringBatchCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbatchcore/SpringBatchCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
